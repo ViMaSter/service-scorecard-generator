f050dd3b029bc57a58469b31fa71f9a7abd92cb1
Vincent <vincent@mahn.ke>
2023-02-24 01:03:37 +0100

    fix(NoNullableElement): Handle missing <Nullable> element

---
+++ ScorecardGenerator.Test/Checks/NullableSetup/NoNullableElement.cs
@@ -0,0 +1,16 @@
+using ScorecardGenerator.Test.Helper;
+using Serilog;
+
+namespace ScorecardGenerator.Test.Checks.NullableSetup;
+
+public class NoNullableElement : TestWithNeighboringCsprojFixture
+{
+    [Test]
+    public void Returns0Points()
+    {
+        var logger = new LoggerConfiguration().CreateLogger();
+        var check = new ScorecardGenerator.Checks.NullableSetup.Check(logger);
+        var deductions = check.SetupLoggerAndRun(AbsolutePathToProjectFile);
+        deductions.CountAndFinalScore(1, 0);
+    }
+}
